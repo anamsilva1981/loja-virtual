8e4eaed8011601afa815fd43609d39680819760a
ana-msilva <ana.msilva@ibm.com>
2023-11-02 00:38:47 -0300

    [feat] add header component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '50%': '50%',
+      '16': '4rem',
     },
-    extend: {},
   },
   plugins: [],
 }
