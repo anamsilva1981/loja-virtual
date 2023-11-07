8daf9312dea16689fb8368ecae9e7277f741b9c2
ana-msilva <ana.msilva@ibm.com>
2023-11-06 22:46:11 -0300

    [feat] finish

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./header.component.scss'],
     standalone: true
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+// import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
@@ -13,6 +13,8 @@ if (environment.production) {
   enableProdMode();
 }
 
+
+
 bootstrapApplication(AppComponent, {
   providers: [
       importProvidersFrom(BrowserModule, AppRoutingModule,  FormsModule),
@@ -21,3 +23,11 @@ bootstrapApplication(AppComponent, {
   ]
 })
   .catch(err => console.error(err));
+function provideHttpClient(arg0: any): import("@angular/core").Provider | import("@angular/core").ImportedNgModuleProviders {
+  throw new Error('Function not implemented.');
+}
+
+function withInterceptorsFromDi(): any {
+  throw new Error('Function not implemented.');
+}
+
